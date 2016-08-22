e52130cdf5af8cfe33d3b4d790c3f357ccfdd54d
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:34:13 -0300

    Ouvindo webhook

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ require 'vendor/autoload.php';
 
 $botToken = "184167750:AAGEJ6DQZKW9WHwJaNUwXffBLn6izGucQ-k";
 $url = "https://api.telegram.org/bot".$botToken;
-// $update = json_decode(file_get_contents('php://input'));
+
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
 
 // $chatId = $update["message"]["chat"]["id"];
 // $message = $update["message"]["text"];
