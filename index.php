d6ee3050fb75fc5c7d741f7f4e3e42ba95f01402
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:50:07 -0300

    Fix sendMessage url

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ switch($message){
 }
 
 function sendMessage ($chatId, $message){
-    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
+    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
     file_get_contents($url);
 }
 
