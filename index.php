b8e234f452458bbeaa88719cc7df02352f2276f1
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:04:16 -0300

    Add index.php file

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$botTokem = getenv('TELEGRAM-TOKEN');
+$url = "https://api.telegram.org/bot".$botTokem;
+$update = json_decode(file_get_contents('php://input'));
+
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+function sendMessage ($chatId, $message){
+    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
+    file_get_contents($url);
+}
+
+try {
+
+    switch($message){
+        case "/teste":
+            sendMessage($chatId, "Sou um teste !"); break;
+        case "/haha":
+            sendMessage($chatId, "HAHAHAHAHA"); break;
+        default
+            break;
+    }
+} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
+    sendMessage($chatId, "Ocorreu um erro");
+}
