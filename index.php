3644c17f96755e7cb92c4697ede329e5033268fe
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:36:07 -0300

    Versão beta do bot do telegram

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "Hello World";
-
 require 'vendor/autoload.php';
 
 $botToken = "184167750:AAGEJ6DQZKW9WHwJaNUwXffBLn6izGucQ-k";
@@ -10,30 +8,30 @@ $url = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
 
-// $chatId = $update["message"]["chat"]["id"];
-// $message = $update["message"]["text"];
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
 
-// function sendMessage ($chatId, $message){
-//     $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
-//     file_get_contents($url);
-// }
+function sendMessage ($chatId, $message){
+    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
+    file_get_contents($url);
+}
 
-// try {
+try {
 
-//     switch($message){
-//         case "/teste":
-//             sendMessage($chatId, "Sou um teste !"); break;
-//         case "/haha":
-//             sendMessage($chatId, "HAHAHAHAHA"); break;
-//         default
-//             break;
-//     }
-// } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-//     sendMessage($chatId, "Ocorreu um erro");
-// }
+    switch($message){
+        case "/teste":
+            sendMessage($chatId, "Sou um teste !"); break;
+        case "/haha":
+            sendMessage($chatId, "HAHAHAHAHA"); break;
+        default
+            break;
+    }
+} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
+    sendMessage($chatId, "Ocorreu um erro");
+}
 
 ?>
 
-<h2>Telegram Bot Teste</h2>
+<h2>Telegram Bot Teste - v0</h2>
 
 <h4>Comandos disponíveis: '/teste' e '/haha'</h4>
