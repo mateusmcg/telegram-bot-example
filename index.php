6fb689550f3b84e4c8d85338e980857686f1172c
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:44:32 -0300

    Adicionando tratamento de erro do PHP

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
 
-require 'vendor/autoload.php';
+init_set('error_reporting', E_ALL);
 
 $botToken = "184167750:AAGEJ6DQZKW9WHwJaNUwXffBLn6izGucQ-k";
-$url = "https://api.telegram.org/bot".$botToken;
+$website = "https://api.telegram.org/bot".$botToken;
 
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-
-echo $chatId;
-echo $message;
 
 // try {
 //     switch($message){
@@ -27,10 +24,10 @@ echo $message;
 //     sendMessage($chatId, "Ocorreu um erro");
 // }
 
-// function sendMessage ($chatId, $message){
-//     $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
-//     file_get_contents($url);
-// }
+function sendMessage ($chatId, $message){
+    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
+    file_get_contents($url);
+}
 
 ?>
 
