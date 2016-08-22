4468eefe63f4788fe692bb7e4530b9f10e711243
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:46:54 -0300

    Fix and deploy telegram bot v0

--- index.php
+++ index.php
@@ -9,18 +9,14 @@ $update = json_decode($update, TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
-// try {
-//     switch($message){
-//         case "/teste":
-//             sendMessage($chatId, "Sou um teste !"); break;
-//         case "/haha":
-//             sendMessage($chatId, "HAHAHAHAHA"); break;
-//         default
-//             break;
-//     }
-// } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-//     sendMessage($chatId, "Ocorreu um erro");
-// }
+switch($message){
+    case "/teste":
+        sendMessage($chatId, "Sou um teste !"); break;
+    case "/haha":
+        sendMessage($chatId, "HAHAHAHAHA"); break;
+    default
+        break;
+}
 
 function sendMessage ($chatId, $message){
     $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
