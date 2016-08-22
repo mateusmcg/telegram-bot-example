370d1991e36245cde9e39cecc04eab7318f28830
Mateus Cerqueira <mateus_mcg@icloud.com>
2016-08-22 01:38:21 -0300

    Recuperando chatId e Mensagem

--- index.php
+++ index.php
@@ -11,24 +11,23 @@ $update = json_decode($update, TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
-function sendMessage ($chatId, $message){
-    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
-    file_get_contents($url);
-}
+// try {
+//     switch($message){
+//         case "/teste":
+//             sendMessage($chatId, "Sou um teste !"); break;
+//         case "/haha":
+//             sendMessage($chatId, "HAHAHAHAHA"); break;
+//         default
+//             break;
+//     }
+// } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
+//     sendMessage($chatId, "Ocorreu um erro");
+// }
 
-try {
-
-    switch($message){
-        case "/teste":
-            sendMessage($chatId, "Sou um teste !"); break;
-        case "/haha":
-            sendMessage($chatId, "HAHAHAHAHA"); break;
-        default
-            break;
-    }
-} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-    sendMessage($chatId, "Ocorreu um erro");
-}
+// function sendMessage ($chatId, $message){
+//     $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text".urlencode($message);
+//     file_get_contents($url);
+// }
 
 ?>
 
